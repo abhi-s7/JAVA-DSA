d00f0683e304723c5cfbb7c91b3a7e4b28c808e7
Abhishek Sharma <68411618+prodev16@users.noreply.github.com>
2020-07-17 10:38:02 +0530

    Update ReversePolishNotation2ndApproach.java

--- StacksAndQueue/ReversePolishNotation2ndApproach.java
+++ StacksAndQueue/ReversePolishNotation2ndApproach.java
@@ -1,6 +1,6 @@
 package stackANDqueue;
 
-public class ReversePolishNotationSirApproach {
+public class ReversePolishNotation2ndApproach {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
