8b06e5a46e30cdaf34a000e8c094c926b37510f8
Abhishek Sharma <68411618+prodev16@users.noreply.github.com>
2020-07-17 10:40:18 +0530

    StacksAndQueue Initial Commit

---
+++ StacksAndQueue/StackArray.java
@@ -0,0 +1,69 @@
+package stackANDqueue.Revise;
+
+class StackArray{
+	
+	private int[] array;
+	private int top;
+	private final int DEFAULT_CAPACITY = 10;
+	
+	StackArray(){
+		array = new int[DEFAULT_CAPACITY];
+		top = -1;
+	}
+	
+	StackArray(int capacity){
+		if(capacity<1) {
+			throw new RuntimeException("Invalid Capacity");
+		}
+		
+		top = -1;
+		array = new int[capacity];
+	}
+	
+	public int getSize() {
+		return top+1;
+	}
+	
+	public boolean isEmpty() {
+		return getSize() == 0;
+	}
+	
+	public void push(int data) {
+		if(getSize() == array.length) {
+			throw new RuntimeException("Stack is full");
+		}
+		
+		top++;
+		array[top] = data;
+	}
+	
+	public int pop() {
+		if(isEmpty()) {
+			throw new RuntimeException("Stack is empty");
+		}
+		
+		int value = array[top];
+		
+		array[top] = 0;
+		top--;
+		return value;
+	}
+	
+	public int topElement() {
+		if(getSize() == 0) {
+			throw new RuntimeException("Stack is empty cannot pick top element");
+		}
+		
+		return array[top];
+	}
+	
+	public void display() {
+		if(isEmpty()) {
+			throw new RuntimeException("Stack is empty");
+		}
+		
+		for(int i = top; i>=0; i--) {
+			System.out.println("Element at position: " + i + " is: " + array[i])	;
+		}
+	}
+}
