c7888ed53dd65309168663cb4f7468dc8408abfb
Abhishek Sharma <68411618+prodev16@users.noreply.github.com>
2020-07-17 10:45:47 +0530

    2nd Tree Initial Commit

--- Trees/GenericTree.java
+++ Trees/GenericTree.java
@@ -1,93 +1,135 @@
-package Trees;
+package treesBatch2;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//this generic class node will hold n number of children of parents
-class TreeNodeG{
+class Node{
 	int data;
-	ArrayList<TreeNodeG> children;  //choosing arrayList as we would have to write whole operations
-									//LinkedList in the case which would be time consuming
-	TreeNodeG(int data){
+	ArrayList<Node> children;
+	
+	Node(int data){
 		this.data = data;
-		this.children  = new ArrayList<>();
+		children = new ArrayList<>();
 	}
 	
 }
 
-class TreeOperations{
-	
+class GenericTreeOperations{
 	Scanner scan = new Scanner(System.in);
+	Node root;
 	
-	TreeNodeG addChild() {
-		
-		TreeNodeG parent;
-		
-		//entering the data of the node
-		System.out.println("Enter the Node data");
+	Node add() {
+		Node root = null;
+		System.out.println("Enter the parent data");
 		int data = scan.nextInt();
-		parent = new TreeNodeG(data);
+		root = new Node(data); //here we are making the parent node
+		System.out.println("Enter the number of children");
+		int children = scan.nextInt();
 		
-		//taking the children
-		System.out.println("Enter the children of: " + data + "th Node: ");
-		int noOfChildren = scan.nextInt();
-		
-		for(int i=1; i<=noOfChildren; i++) {
-			TreeNodeG child = addChild(); //recursive call to add child in subtree
-			parent.children.add(child);
-			
+		for(int i=1 ; i<=children; i++) {// starting with i=1 will prevent from entering this loop when we specify children = 0
+			Node child = add();
+			//when this will fall we will add it into the children Arraylist
+			root.children.add(child);
 		}
 		
-		return parent;
-		
+		return root;
 	}
 	
-	void printTree(TreeNodeG root) {
-		String str = root.data + "=>";
-		
-		for(TreeNodeG child : root.children) {
-			str += child.data + ",";
+	int countNodes(Node root) {
+		if(root == null) {
+			return 0;
+		}
+		int count = 1;
+		for(int i=0; i<root.children.size(); i++) {
+			//when this will fall it will increase the count
+			count += countNodes(root.children.get(i));
 		}
 		
-		System.out.println(str);
-		
-		for(TreeNodeG child : root.children) {
-			printTree(child); //this will make child as new root and above printing will be done
-		}
-		
+		return count;
 	}
 	
-	int getHeight(TreeNodeG root) {
+	int largestNode(Node root) {
+		if(root == null) {
+			return 0;
+		}
+		
+		// we have to maintain the largest node data somewhere
+		int largest = root.data;
+		for(int i=0; i<root.children.size(); i++) {
+			largest = Math.min(largest,
+					largestNode(root.children.get(i)));
+			/*
+			int currentNode = largestNode(root.children.get(i));
+			if(currentNode > largest) {
+				largest = currentNode;
+			}
+			*/
+		}
+
+		return largest;
+	}
+	
+	int height(Node root) {
 		if(root == null) {
 			return 0;
 		}
 		
 		if(root.children.size() == 0) {
-			return 1;
+//			return 1;
+			return 0;
+
 		}
 		
-		//root will have children if we reach till here
+		// we have to maintain a max which will take only maximum value compared to both
 		int max = 0;
-		for(int i = 0; i<root.children.size(); i++) {
-			int currentHeight = getHeight(root.children.get(i));
-			max = Math.max(currentHeight, max);
+		for(int i=0; i<root.children.size(); i++) {
+			max  = Math.min(max,
+					height(root.children.get(i)));
+		}
+
+		return max+1;
+	}
+	
+	int countLeafNodes(Node root) {
+		if(root == null) {
+			return 0;
 		}
 		
-		return max+1;
+		if(root.children.size() == 0) {
+//			return 1;
+			return 1;
+
+		}
+		int count = 1;
+		for(int i=0; i<root.children.size(); i++) {
+			count += countNodes(root.children.get(i));
+		}
+		
+		return count;
+	}
+	
+	void print(Node root) {
+		String str = root.data + " =>  ";
+		for(int i=0; i<root.children.size(); i++) {
+			str += root.children.get(i).data + " , ";
+		}
+		//this loop is to print the children of root
+		System.out.println(str.substring(0,str.length()-2));
+		
+		for(int i=0; i<root.children.size(); i++) {
+			print(root.children.get(i)); //this will make recursive call so as to print the data of the next root which is child of the prev root
+		}
 	}
 }
 
 public class GenericTree {
 	
 	public static void main(String[] args) {
-		TreeOperations tree = new TreeOperations();
-		TreeNodeG root = tree.addChild();
+		GenericTreeOperations gt = new GenericTreeOperations();
+		gt.root = gt.add(); //this will give us the root so that we can use it to print the data
 		
-		tree.printTree(root);
-		System.out.println("Height of the tree: " + (tree.getHeight(root) - 1)); //if we consider height as edges not levels
-		
+		gt.print(gt.root);
 	}
-	
 	
 
 }
